6ec1d30490edcbf16b40926f25c6f39966a1ed24
pshubham <pshubham@salesforce.com>
2024-09-28 13:20:18 +0530

    Adding linter and github workflows

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,6 @@
 import type { Metadata } from "next";
-// import localFont from "next/font/local";
 import "@/styles/globals.css";
-
-// const geistSans = localFont({
-//   src: "./fonts/GeistVF.woff",
-//   variable: "--font-geist-sans",
-//   weight: "100 900",
-// });
-// const geistMono = localFont({
-//   src: "./fonts/GeistMonoVF.woff",
-//   variable: "--font-geist-mono",
-//   weight: "100 900",
-// });
+import React from "react";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -26,7 +15,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        // className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
       </body>
