df30a8d3978401f967539c20248c2e4c62d4dc27
rishavvajpayee <reshavvajpayee@gmail.com>
2024-09-29 16:19:14 +0530

    implemented Search with static diceCmdData

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        'Assistant': ['Assistant'],
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
