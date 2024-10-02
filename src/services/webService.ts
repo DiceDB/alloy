af13ee6a4673da09223f5e662b477a626aa9709d
tarun-29 <29kantiwaltarun@gmail.com>
2024-10-02 21:07:15 +0530

    fix: fallback if .env not present

--- src/services/webService.ts
+++ src/services/webService.ts
@@ -1,6 +1,12 @@
 import axios from "axios";
-const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
+let BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
 
+if (!BACKEND_URL) {
+  console.warn(
+    "Warning: NEXT_PUBLIC_BACKEND_URL is not defined. Defaulting to http://localhost:3000"
+  );
+  BACKEND_URL = "http://localhost:8080";
+}
 export const WebService = axios.create({
   baseURL: BACKEND_URL,
   headers: {
