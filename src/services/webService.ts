f219473c6fb1b9ed04846148ff3af0bd586b485c
tarun-29 <29kantiwaltarun@gmail.com>
2024-10-02 12:43:26 +0530

    fix: access root_url from .env file

--- src/services/webService.ts
+++ src/services/webService.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { ROOT_URL } from "@/shared/constants/apiEndpoints";
+const ROOT_URL = process.env.NEXT_PUBLIC_ROOT_URL;
 
 export const WebService = axios.create({
   baseURL: ROOT_URL,
