acd6c7aecdf94a43cb30e7eb0a07e0c3e6e34a39
Prathamesh Koshti <prathamesh.koshti@deepintent.com>
2024-10-03 22:44:46 +0530

    refactor: remove unused comment

--- jest.config.ts
+++ jest.config.ts
@@ -10,11 +10,10 @@ const createJestConfig = nextJest({
 const config: Config = {
   coverageProvider: 'v8',
   testEnvironment: 'jsdom',
-  // Add more setup options before each test is run
-  // setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
+  // Add more setup options before each test is run
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
 };
 
