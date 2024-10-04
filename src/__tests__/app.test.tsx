85c34615103986f8cb5f5d5c297dc326caa1ff09
Prathamesh Koshti <prathamesh.koshti@deepintent.com>
2024-10-04 11:24:58 +0530

    fix: prettier formatting

--- src/__tests__/app.test.tsx
+++ src/__tests__/app.test.tsx
@@ -1,6 +1,6 @@
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
-import Page from '../app/page'
+import Page from '../app/page';
 
 describe('Render Page', () => {
   it('should render app component', async () => {
