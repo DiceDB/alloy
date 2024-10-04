1bbc6d4537ab8edac802a943b9570718bdfd0e03
rishavvajpayee <reshavvajpayee@gmail.com>
2024-10-05 00:31:22 +0530

    updated the BE url endpoint

--- src/shared/constants/apiEndpoints.ts
+++ src/shared/constants/apiEndpoints.ts
@@ -1,2 +1,2 @@
 // This folder will contain or hold constants. For example in this particular file we can store the api endpoints as constants
-export const CLI_COMMAND_URL = 'http://localhost:8080/cli';
+export const CLI_COMMAND_URL = 'http://localhost:8080/shell/exec';
