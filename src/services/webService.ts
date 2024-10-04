26a1951551328c3c4b48195e0af2779f8a5316c5
tarun-29 <29kantiwaltarun@gmail.com>
2024-10-04 16:21:24 +0530

    prettier check fix

--- src/services/webService.ts
+++ src/services/webService.ts
@@ -1,15 +1,15 @@
-import axios from "axios";
+import axios from 'axios';
 let PLAYGROUND_MONO_URL = process.env.NEXT_PUBLIC_PLAYGROUND_MONO_URL;
 
 if (!PLAYGROUND_MONO_URL) {
   console.warn(
-    "Warning: NEXT_PUBLIC_BACKEND_URL is not defined. Defaulting to http://localhost:3000"
+    'Warning: NEXT_PUBLIC_BACKEND_URL is not defined. Defaulting to http://localhost:3000',
   );
-  PLAYGROUND_MONO_URL = "http://localhost:8080";
+  PLAYGROUND_MONO_URL = 'http://localhost:8080';
 }
 export const WebService = axios.create({
   baseURL: PLAYGROUND_MONO_URL,
   headers: {
-    "Content-Type": "application/json",
+    'Content-Type': 'application/json',
   },
 });
