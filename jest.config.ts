d4787221a0b5e1ab3f3be0a233bd6ba106cd09bd
Prathamesh Koshti <prathamesh.koshti@deepintent.com>
2024-10-04 08:49:20 +0530

    chore: formatting and update package lock file

--- jest.config.ts
+++ jest.config.ts
@@ -1,5 +1,5 @@
-import type { Config } from 'jest'
-import nextJest from 'next/jest.js'
+import type { Config } from 'jest';
+import nextJest from 'next/jest.js';
 
 const createJestConfig = nextJest({
   // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
