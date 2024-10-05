68880ad5614a71da67a6879251dd031c6d10c78b
tarun-29 <29kantiwaltarun@gmail.com>
2024-10-05 16:52:22 +0530

    fix: just use fetch nothing else

--- src/services/webService.ts
+++ src/services/webService.ts
@@ -1,4 +1,3 @@
-import axios from 'axios';
 let PLAYGROUND_MONO_URL = process.env.NEXT_PUBLIC_PLAYGROUND_MONO_URL;
 
 if (!PLAYGROUND_MONO_URL) {
@@ -7,9 +6,58 @@ if (!PLAYGROUND_MONO_URL) {
   );
   PLAYGROUND_MONO_URL = 'http://localhost:8080';
 }
-export const WebService = axios.create({
-  baseURL: PLAYGROUND_MONO_URL,
-  headers: {
-    'Content-Type': 'application/json',
+
+type HeadersType = {
+  [key: string]: string;
+};
+
+type RequestOptions = {
+  method: string;
+  headers: Record<string, string>;
+  body?: string; // Optional because not all requests will have a body
+};
+
+export const webService = {
+  request: async (
+    url: string,
+    method: string,
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    data: any = null,
+    headers: HeadersType,
+  ) => {
+    const options: RequestOptions = {
+      method,
+      headers: {
+        'Content-Type': 'application/json',
+        ...headers,
+      },
+    };
+
+    // Only add 'body' if the method is not GET and if data is present
+    if (data && method !== 'GET') {
+      options.body = JSON.stringify(data);
+    }
+
+    try {
+      const response = await fetch(url, options);
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      const result = await response.json();
+      return result;
+    } catch (error) {
+      if (error instanceof Error)
+        console.error(`Error with ${method} request: ${error.message}`);
+      throw error;
+    }
   },
-});
+
+  get: (url: string, headers: HeadersType = {}) => {
+    return webService.request(url, 'GET', null, headers);
+  },
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  post: (url: string, data: any, headers: HeadersType = {}) => {
+    return webService.request(url, 'POST', data, headers);
+  },
+};
