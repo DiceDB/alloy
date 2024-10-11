17d5aef3648c5b77ce2676202db2e15321a2697b
Prashant Shubham <prashant1996cr07@gmail.com>
2024-10-11 12:48:22 +0530

    Add external links to footer items (#68)

--- apps/playground-web/components/Footer/Footer.tsx
+++ apps/playground-web/components/Footer/Footer.tsx
@@ -5,6 +5,7 @@ import { Twitter } from '@mui/icons-material';
 import GitHub from '@mui/icons-material/GitHub';
 import People from '@mui/icons-material/People';
 import { Button } from '@dicedb/ui/button';
+import Link from 'next/link';
 
 export default function Footer() {
   return (
@@ -12,76 +13,108 @@ export default function Footer() {
       <div className="container mx-auto px-4">
         <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
           <div>
-            <h3 className="text-gray-500 font-semibold mb-4">DiceDB</h3>
-            <Button className="w-full !bg-red-600 hover:!bg-red-700 !text-white">
-              Get Started →
-            </Button>
-            <Button
-              variant="outline"
-              className="!w-full mt-2 !border-1 !border-gray-700 bg-blue-50 hover:text-blue text-black hover:text-blue-600"
+            <h3 className="text-gray-500 font-semibold mb-4 text-center">
+              DiceDB
+            </h3>
+            <Link
+              href="https://dicedb.io/get-started/installation/"
+              target="_blank"
             >
-              <GitHub className="mr-2 h-4 w-4" /> GitHub (4k+)
-            </Button>
+              <Button className="w-full !bg-red-600 hover:!bg-red-700 !text-white">
+                Get Started →
+              </Button>
+            </Link>
+            <Link href="https://github.com/dicedb/dice" target="_blank">
+              <Button
+                variant="outline"
+                className="!w-full mt-2 !border-1 !border-gray-700 bg-blue-50 hover:text-blue text-black hover:text-blue-600"
+              >
+                <GitHub className="mr-2 h-4 w-4" /> GitHub (4k+)
+              </Button>
+            </Link>
           </div>
 
-          <div>
+          <div className="text-center">
             <h3 className="text-gray-500 font-semibold mb-4">Developers</h3>
             <ul className="space-y-2">
               <li>
-                <a href="#" className="text-gray-600 hover:text-gray-900">
+                <a
+                  href="https://dicedb.io/get-started/installation"
+                  target="_blank"
+                  className="text-gray-600 hover:text-gray-900"
+                >
                   Quickstart
                 </a>
               </li>
               <li>
-                <a href="#" className="text-gray-600 hover:text-gray-900">
+                <a
+                  href="https://dicedb.io/commands/get"
+                  target="_blank"
+                  className="text-gray-600 hover:text-gray-900"
+                >
                   Commands
                 </a>
               </li>
               <li>
-                <a href="#" className="text-gray-600 hover:text-gray-900">
+                <a
+                  href="https://github.com/DiceDB/dice/tree/master/examples/leaderboard-go"
+                  target="_blank"
+                  className="text-gray-600 hover:text-gray-900"
+                >
                   Examples
                 </a>
               </li>
             </ul>
           </div>
 
-          <div>
+          <div className="text-center">
             <h3 className="text-gray-500 font-semibold mb-4">Examples</h3>
             <ul className="space-y-2">
               <li>
-                <a href="#" className="text-gray-600 hover:text-gray-900">
+                <a
+                  href="https://github.com/DiceDB/dice/tree/master/examples/leaderboard-go"
+                  target="_blank"
+                  className="text-gray-600 hover:text-gray-900"
+                >
                   Real-time Leaderboard
                 </a>
               </li>
             </ul>
           </div>
 
-          <div>
+          <div className="text-center">
             <h3 className="text-gray-500 font-semibold mb-4">Us and Socials</h3>
             <ul className="space-y-2">
               <li>
-                <a href="#" className="text-gray-600 hover:text-gray-900">
+                <a
+                  href="mailto:arpit@dicedb.io"
+                  target="_blank"
+                  className="text-gray-600 hover:text-gray-900"
+                >
                   Contact Us
                 </a>
               </li>
             </ul>
-            <div className="flex space-x-4 mt-4">
+            <div className="space-x-4 mt-4 items-center justify-items-center">
               <a
-                href="#"
+                href="https://discord.gg/6r8uXWtXh7"
+                target="_blank"
                 className="text-gray-400 hover:text-gray-600"
                 aria-label="People"
               >
                 <People className="h-6 w-6" />
               </a>
               <a
-                href="#"
+                href="https://twitter.com/thedicedb"
+                target="_blank"
                 className="text-gray-400 hover:text-gray-600"
                 aria-label="Twitter"
               >
                 <Twitter className="h-6 w-6" />
               </a>
               <a
-                href="#"
+                href="https://github.com/dicedb/dice"
+                target="_blank"
                 className="text-gray-400 hover:text-gray-600"
                 aria-label="GitHub"
               >
