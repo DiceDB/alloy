2ddaa008996c758ecd54bfd336839d84ebc0f436
Ajay Poshak <poshakajay@gmail.com>
2024-10-12 15:46:36 +0530

    fix formatting

--- apps/console-electron/src/pages/index.tsx
+++ apps/console-electron/src/pages/index.tsx
@@ -18,7 +18,7 @@ export default function Home() {
       className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
     >
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <h1 className='lg'>DiceDB</h1>
+        <h1 className="lg">DiceDB</h1>
         <img
           width={48}
           height={48}
