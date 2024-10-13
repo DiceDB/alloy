e622df0bc749dabe9bf933993aba8a357283c23e
Ajay Poshak <poshakajay@gmail.com>
2024-10-13 15:33:36 +0530

    Fix ESlint setup

--- apps/console-electron/.eslintrc.mjs
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  root: true,
-  extends: ['@dicedb/eslint-config/next.js', 'next/typescript'],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: true, // To enable type-aware linting
-  },
-  ignorePatterns: ['out'],
-};
