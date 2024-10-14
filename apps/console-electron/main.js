3ec695452c8e4a032a7ff60e83233031cf97b5fc
Ajay Poshak <poshakajay@gmail.com>
2024-10-14 21:49:35 +0530

    Add background color to main screen

--- apps/console-electron/main.js
+++ apps/console-electron/main.js
@@ -2,8 +2,14 @@ import { app, BrowserWindow } from 'electron';
 
 function createWindow() {
   const win = new BrowserWindow({
+    show: false,
     useContentSize: true,
+    backgroundColor: '#0a0a0a',
   });
+
+  win.once('ready-to-show', () => {
+    win.show()
+  })
 
   if (app.isPackaged === false) {
     win.loadURL('http://localhost:4000');
