f3b0f1810dc5de2a8349585b82368e07f8573adb
ViragJain3010 <viragjain3010@gmail.com>
2024-10-16 19:46:24 +0530

    Created tests for components/Header

--- apps/playground-web/components/Header/Header.tsx
+++ apps/playground-web/components/Header/Header.tsx
@@ -2,7 +2,7 @@
 
 export default function Header() {
   return (
-    <header className="flex items-center mb-4">
+    <header data-testid="header" className="flex items-center mb-4">
       <h1 className="text-2xl font-bold">DiceDB PlayGround</h1>
     </header>
   );
